e29bcea85efc8f87276c00767c9600f19d1cb5d7
Bikash <bikashchaudhary007@gmail.com>
2022-11-15 15:49:23 +0545

    Multipication of two numbers

---
+++ multiplication.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+  int a,b,mult;
+  printf("Enter first number: ");
+  scanf("%d",&a);
+
+  printf("Enter second number: ");
+  scanf("%d",&b);
+
+  mult = a * b ;
+
+  printf("The resultant value is: %d",mult);
+
+
+}
